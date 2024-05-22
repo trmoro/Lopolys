f213d0b7fde37dec94d7b62f0a0899ef36e828e2
trmoro <tristan.moro@outlook.fr>
2024-05-22 19:29:28 +0200

    Threejs with no addons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import * as THREE from 'three';
-import { TrackballControls } from 'three-addons';
+//import { TrackballControls } from 'three-addons';
 
 @Component({
   selector: 'app-root',
@@ -39,12 +39,13 @@ export class AppComponent implements AfterViewInit{
     renderer.setAnimationLoop( animation );
     document.body.appendChild( renderer.domElement );
 
+    /*
     const controls = new TrackballControls( camera, renderer.domElement );
     controls.rotateSpeed = 1.0;
     controls.zoomSpeed = 1.2;
     controls.panSpeed = 0.8;
     controls.keys = [ 'KeyA', 'KeyS', 'KeyD' ];
-
+    */
 
     // animation
 
