f45afd4046da0369828f487c91b0c1d8e7c0a280
trmoro <tristan.moro@outlook.fr>
2024-05-22 23:28:39 +0200

    window resize and cube loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,41 +18,65 @@ export class AppComponent implements AfterViewInit{
   title = 'Lopolys';
   constructor(private window: Window){}
 
+  //After view init
   ngAfterViewInit(): void {
+    
     const width = window.innerWidth, height = window.innerHeight;
 
     // init
-
     const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
     camera.position.z = 1;
 
     const scene = new THREE.Scene();
-
-    const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-    const material = new THREE.MeshNormalMaterial();
-
-    const mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
 
     const renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( width, height );
     renderer.setAnimationLoop( animation );
     document.body.appendChild( renderer.domElement );
 
+    //Camera control
     const controls = new TrackballControls( camera, renderer.domElement );
     controls.rotateSpeed = 1.0;
     controls.zoomSpeed = 1.2;
     controls.panSpeed = 0.8;
     controls.keys = [ 'KeyA', 'KeyS', 'KeyD' ];
 
-    // animation
+    //On window resize
+    function onWindowResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+    window.addEventListener( 'resize', onWindowResize );
 
+    //Instantiate Cube
+    let cubes : THREE.Mesh[] = [];
+    function instantiateCube() {
+      const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+      const material = new THREE.MeshNormalMaterial();
+      const mesh = new THREE.Mesh( geometry, material );
+      mesh.position.x = Math.random() / 2;
+      mesh.position.z = Math.random() / 3;
+      scene.add( mesh );
+      cubes.push(mesh);
+
+      if(cubes.length > 20) {
+        scene.remove(cubes[0]);
+        cubes.shift();
+      }
+    }
+    setInterval(instantiateCube, 150);
+
+    //Animation
     function animation( time: number ) {
 
       requestAnimationFrame( animation );
 
-      mesh.rotation.x = time / 5000;
-      mesh.rotation.y = time / 4000;
+      cubes.forEach(c => {
+        c.rotation.x = time / 5000;
+        c.rotation.y = time / 4000;  
+        c.position.y += 1 / 8000;
+      });
 
       controls.update();
 
